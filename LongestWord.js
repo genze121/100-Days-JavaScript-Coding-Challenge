d20a1c751ca2c046fd0cc9f740e8a9d9f9c63f20
Tirtha Sharma <tirthagenze121@gmail.com>
2024-04-17 13:56:16 +0530

    LongestWord code.

--- LongestWord.js
+++ LongestWord.js
@@ -13,17 +13,25 @@
 //? If the input string is empty or contains only whitespae, the function should return an false.
 //? The function should ignore leading and trailing whitepace when determining the longest word.
 
-const str =
-  "Watch Thapa Technical javascript ygfjhewgfdagfgdalfgadFGKADGFGDFKLGDKFGKDGFKGDFKLGDAKFGDKFGKDGFLDGKJHGDKLFGDKFG course on youtube";
+const str = "Interview preparation for faang companies";
 
-// const str = "";
 function findLongestWord(str) {
+  // Conditional check for empty string
   if (str.trim().length === 0) return "Empty String";
 
   let words = str.split(" ");
 
+  // 1st approach using sort() function.
   const finalWords = words.sort((a, b) => b.length - a.length);
   console.log(finalWords[0]);
+
+  // 2nd approach using reduce() function.
+  const reduceWords = words.reduce(
+    (accumulator, word) =>
+      word.length > accumulator.length ? word : accumulator,
+    ""
+  );
+  console.log(reduceWords);
 }
 
 console.log(findLongestWord(str));
