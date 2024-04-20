e86fdadd5809b77a36d8eea05a1bc88f3b975f30
Tirtha Sharma <tirthagenze121@gmail.com>
2024-04-20 21:43:51 +0530

    Camel and Snake case.

---
+++ CamelSnakeCase.js
@@ -0,0 +1,38 @@
+//* ______________________________________________________________________
+//* Programming Question : Convert string into Camel case and Snake case.
+//* ______________________________________________________________________
+
+//? Write a function to convert a string to camelcase and snake case.
+
+function toCamelCase(str) {
+  return str
+    .trim()
+    .split(" ")
+    .map((element, index) => {
+      if (index === 0) {
+        return element;
+      } else {
+        return (
+          element.charAt(0).toUpperCase() + element.slice(index).toLowerCase()
+        );
+      }
+    })
+    .join(" ");
+}
+
+function toSnakeCase(str) {
+  return str
+    .trim()
+    .split(" ")
+    .map((element, index) => {
+      if (index === 0) {
+        return element;
+      } else {
+        return element.charAt(0) + element.slice(index);
+      }
+    })
+    .join("_");
+}
+
+console.log(toCamelCase("hello world")); // camel case => hello World
+console.log(toSnakeCase("hello world"));
